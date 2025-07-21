8921cc903a2ca81744b11b2386666e1a73ba4c61
JayeshHadke <jayeshhadkejrh@gmail.com>
2025-07-22 00:49:40 +0530

    Comment out NotifyAccess function and its imports for future reference

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import MainPage from "@/components/mainPage";
-import NotifyAccess from "@/services/notify";
+// import NotifyAccess from "@/services/notify";
 import Footers from "@/components/footers";
 import AccessWarningBanner from "@/components/TopBanner";
 export default function Home() {
   return (
     <div>
-      {NotifyAccess()}
+      {/* {NotifyAccess()} */}
       <AccessWarningBanner />
       <MainPage />
       <Footers />
