92e22117989d696a21df52f6be2721f356b75e84
JayeshHadke <jayeshhadkejrh@gmail.com>
2025-08-06 15:43:15 +0530

    Add new SVG icons and update section details
    - Added SVG icons for Angular, C#, Java, LangChain, and LangGraph.
    - Updated sectionDetails.json to include new skills and their corresponding icons.
    - Modified cmdContainer.tsx to notify the author upon form submission.
    - Refactored notify.tsx to send email notifications with user data.

--- my-portfolio/src/services/notify.tsx
+++ my-portfolio/src/services/notify.tsx
@@ -1,107 +1,27 @@
-// import { useEffect } from "react";
-// import emailjs from "emailjs-com";
-// import { error } from "console";
+import { useEffect } from "react";
+import emailjs from "emailjs-com";
 
-// export default function NotifyAccess() {
-//   useEffect(() => {
-//     // Prevent sending again in this session
-//     if (sessionStorage.getItem("emailSent") === "true") return;
+export default function notifyAuthor(data: {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}) {
+  const message = ` 📧 Name: ${data.name}
+                    📧 Email: ${data.email}
+                    📞 Phone: ${data.phone}
+                    📝 Message: ${data.message}`;
 
-//     const now = new Date();
-//     const time = now.toLocaleTimeString();
-//     const date = now.toLocaleDateString();
-//     const language = navigator.language;
-
-//     const { browser, os } = parseUserAgent();
-
-//     fetch("https://ipapi.co/json/")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         navigator.geolocation.getCurrentPosition(
-//           (pos) => {
-//             fetch(
-//               `https://nominatim.openstreetmap.org/reverse?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&format=json`
-//             )
-//               .then((res) => res.json())
-//               .then((locationData) => {
-//                 const message = `📍 Location: ${data.city}, ${data.country_name}
-//                         🧠 IP: ${data.ip}
-//                         🌐 Browser: ${browser} on ${os}
-//                         🗣️ Language: ${language}
-//                         🕒 Time: ${time} on ${date}
-//                           Network ORG: ${data.org}
-//                           Region: ${data.region}  
-//                           Location: ${locationData.display_name || "N/A"}
-//                           latitude: ${locationData.lat || "N/A"},
-//                           longitude: ${locationData.lon || "N/A"}`.trim();
-
-//                 emailjs
-//                   .send(
-//                     process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-//                     process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
-//                     { message }, // single variable
-//                     process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
-//                   )
-//                   .then(() => {
-//                     sessionStorage.setItem("emailSent", "true");
-//                     console.log(
-//                       "✅ Admin notified via email with accurate location"
-//                     );
-//                   });
-//               });
-//           },
-//           (error) => {
-//             const message = `📍 Location: ${data.city}, ${data.country_name}
-//                         🧠 IP: ${data.ip}
-//                         🌐 Browser: ${browser} on ${os}
-//                         🗣️ Language: ${language}
-//                         🕒 Time: ${time} on ${date}
-//                           Network ORG: ${data.org}
-//                           Postal: ${data.postal}
-//                           Region: ${data.region}  `.trim();
-
-//             emailjs
-//               .send(
-//                 "service_h03t7ru",
-//                 "template_4x21o89",
-//                 { message }, // single variable
-//                 "QKMpRVnI3_lJPUYtR"
-//               )
-//               .then(() => {
-//                 sessionStorage.setItem("emailSent", "true");
-//                 console.log("✅ Admin notified via email partial location");
-//               });
-//           }
-//         );
-//       });
-//   }, []);
-
-//   const parseUserAgent = () => {
-//     const ua = navigator.userAgent;
-//     const os = ua.includes("Windows")
-//       ? "Windows"
-//       : ua.includes("Mac")
-//       ? "macOS"
-//       : ua.includes("Linux")
-//       ? "Linux"
-//       : ua.includes("Android")
-//       ? "Android"
-//       : ua.includes("iPhone") || ua.includes("iPad")
-//       ? "iOS"
-//       : "Unknown";
-
-//     const browser = ua.includes("Chrome")
-//       ? "Chrome"
-//       : ua.includes("Firefox")
-//       ? "Firefox"
-//       : ua.includes("Safari")
-//       ? "Safari"
-//       : ua.includes("Edg")
-//       ? "Edge"
-//       : "Unknown";
-
-//     return { browser, os };
-//   };
-
-//   return null;
-// }
+  emailjs
+    .send(
+      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+      { message }, // single variable
+      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+    )
+    .then(() => {
+      sessionStorage.setItem("emailSent", "true");
+      console.log("✅ Email sent successfully!");
+    });
+  return null;
+}
