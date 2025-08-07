711e3430ef35362aa2c339ea16c6446c95f5ed0b
JayeshHadke <jayeshhadkejrh@gmail.com>
2025-08-07 07:48:29 +0530

    Add Vercel Analytics integration to MainPage and update dependencies in package.json and package-lock.json

--- my-portfolio/src/components/mainPage.tsx
+++ my-portfolio/src/components/mainPage.tsx
@@ -3,11 +3,15 @@
 import React from "react";
 import CommandBlock from "./commandBlock";
 import CmdWindowPortfolio from "./cmdContainer";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function MainPage() {
   return (
     // <div className="m-4 bg-blue sticky top-16">
+    <div>
+      <Analytics />
       <CmdWindowPortfolio />
+    </div>
     // </div>
   );
 }
